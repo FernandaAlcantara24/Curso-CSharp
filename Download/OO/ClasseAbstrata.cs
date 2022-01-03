cdec61c73970b68dc66642f64cd71e51ab58da33
Fernanda Alcantara <fernanda.alcantara2407@gmail.com>
2022-01-03 11:57:44 -0300

    Orientação a Objetos

---
+++ Download/OO/ClasseAbstrata.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.OO
+{
+    // Classe abstrata pode ou n ter um método abstrato. É uma classe inacabada.
+    public abstract class Celular // Só pode instânciar classes concretas.
+    {
+        public abstract string Assistente();
+
+        public string Tocar()
+        {
+            return "Trim trim trim...";
+        }
+    }
+
+    public class Samsung : Celular
+    {
+        public override string Assistente()
+        {
+            return "Olá! Meu nome é Bixby!";
+        }
+    }
+
+    public class IPhone : Celular
+    {
+        public override string Assistente()
+        {
+            return "Olá! Meu nome é Siri!";
+        }
+    }
+
+    class ClasseAbstrata
+    {
+        public static void Executar()
+        {
+            var celulares = new List<Celular>
+            {
+                new IPhone(),
+                new Samsung()
+            };
+
+            foreach (var celular in celulares)
+            {
+                Console.WriteLine(celular.Assistente());
+            }
+        }
+    }
+}
---
+++ Download/OO/Sealed.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.OO
+{
+    // Classe celada. Só faz sentido usar o Sealed quando o método está marcado como virtual.
+    sealed class SemFilho
+    {
+        public double ValorDaFortuna()
+        {
+            return 1_407_033.65;
+        }
+    }
+    // Não é possível herdar de uma classe celada
+
+    //class SouFilho : SemFilho
+    //{
+
+    //}
+
+    class Avo
+    {
+        public virtual bool HonrarNomeFamilia()
+        {
+            return true;
+        }
+    }
+
+    class Pai : Avo
+    {
+        public override sealed bool HonrarNomeFamilia()
+        {
+            return true;
+        }
+    }
+
+    // Um método celado n pode ser sobrescrito.
+    class FilhoRebelde : Pai
+    {
+        //public new bool HonrarNomeFamilia()
+        //{
+        //    return false;
+        //}
+    }
+
+    class Sealed
+    {
+        public static void Executar()
+        {
+            SemFilho semFilho = new SemFilho();
+            Console.WriteLine(semFilho.ValorDaFortuna());
+
+            FilhoRebelde filho = new FilhoRebelde();
+            Console.WriteLine(filho.HonrarNomeFamilia());
+        }
+    }
+}
